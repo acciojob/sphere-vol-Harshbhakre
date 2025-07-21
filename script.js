c490a436ce8ac0cfd43ebbc55b3e22fa362fb16a
Harsh Bhakre <140039147+Harshbhakre@users.noreply.github.com>
2025-07-21 10:07:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+    let radius = parseFloat(document.querySelector("#radius").value);
+
+    if (isNaN(radius) || radius < 0) {
+        document.querySelector("#volume").value = "NaN";
+        return;
+    }
+
+    let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    document.querySelector("#volume").value = volume.toFixed(4);
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
